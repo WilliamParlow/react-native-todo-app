711b62af3e32a3be7da1e6aa348babde9da4f703
William Parlow <williamnichelatti@catolicasc.org.br>
2020-06-10 00:48:06 -0300

    Created FirebaseConfig again

---
+++ services/FirebaseConfig.js
@@ -0,0 +1,9 @@
+export const FirebaseConfig = {
+    apiKey: "AIzaSyDG8CYwnCadN3xE3piExCeox4x_8Nav6GY",
+    authDomain: "todo-app-2ccf4.firebaseapp.com",
+    databaseURL: "https://todo-app-2ccf4.firebaseio.com",
+    projectId: "todo-app-2ccf4",
+    storageBucket: "todo-app-2ccf4.appspot.com",
+    messagingSenderId: "1045090461961",
+    appId: "1:1045090461961:web:b1bb722f5d2be57c86cbd8"
+};
